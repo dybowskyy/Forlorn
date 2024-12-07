1b7611d70ad6a33987880c7dd3099045ea091669
dybowskyy <igordyb@gmail.com>
2024-12-07 15:33:53 +0100

    Tile generations

--- game.py
+++ game.py
@@ -1,35 +1,39 @@
 import pygame
 import sys
 
+from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
+
 class Game:
     def __init__(self):
         self.image_r = None
         pygame.init()
         pygame.display.set_caption("Forlorn")
         self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
+
         self.clock = pygame.time.Clock()
 
-        self.image = pygame.image.load("data/images/entities/player.png")
-        self.image.set_colorkey((0, 0, 0))
-        self.image_pos = [160, 260]
         self.movement = [False, False, False, False]
-
-        self.collision_area = pygame.Rect(50, 50, 540, 50)
+        # TODO: Optimize self.assets loading.
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png')
+        }
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.tilemap = Tilemap(self)
 
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248))
-            self.image_r = pygame.Rect(*self.image_pos, *self.image.get_size())
+            self.display.fill((14, 219, 248))
+            self.tilemap.render(self.display)
 
-            if self.image_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 255, 0), self.collision_area)
-                #self.movement = [False, False, False, False]
-            else:
-                pygame.draw.rect(self.screen, (255, 0, 0), self.collision_area)
-
-            self.image_pos[1] += (self.movement[1] - self.movement[0]) * 3
-            self.image_pos[0] += (self.movement[2] - self.movement[3]) * 3
-            self.screen.blit(self.image, self.image_pos)
+            self.player.update((self.movement[2] - self.movement[3], self.movement[1] - self.movement[0]))
+            self.player.render(self.display)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -40,6 +44,8 @@ class Game:
                     key_map = {pygame.K_UP: 0, pygame.K_DOWN: 1, pygame.K_RIGHT: 2, pygame.K_LEFT: 3}
                     if event.key in key_map:
                         self.movement[key_map[event.key]] = key_state
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
 
