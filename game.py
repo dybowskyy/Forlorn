f2599c5a6d47542d02994fc672e630eb998657d1
dybowskyy <igordyb@gmail.com>
2024-12-07 13:32:54 +0100

    Movement and collision detection

--- game.py
+++ game.py
@@ -3,8 +3,9 @@ import sys
 
 class Game:
     def __init__(self):
+        self.image_r = None
         pygame.init()
-        pygame.display.set_caption("Game")
+        pygame.display.set_caption("Forlorn")
         self.screen = pygame.display.set_mode((640, 480))
         self.clock = pygame.time.Clock()
 
@@ -13,14 +14,23 @@ class Game:
         self.image_pos = [160, 260]
         self.movement = [False, False, False, False]
 
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+        self.collision_area = pygame.Rect(50, 50, 540, 50)
 
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
+            self.image_r = pygame.Rect(*self.image_pos, *self.image.get_size())
+
+            if self.image_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 255, 0), self.collision_area)
+                #self.movement = [False, False, False, False]
+            else:
+                pygame.draw.rect(self.screen, (255, 0, 0), self.collision_area)
+
             self.image_pos[1] += (self.movement[1] - self.movement[0]) * 3
             self.image_pos[0] += (self.movement[2] - self.movement[3]) * 3
             self.screen.blit(self.image, self.image_pos)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
