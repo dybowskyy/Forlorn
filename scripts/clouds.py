75238f90655a0fbb3d68d714a6f30a85d964d888
dybowskyy <igordyb@gmail.com>
2024-12-09 23:39:11 +0100

    Clouds fix

--- scripts/clouds.py
+++ scripts/clouds.py
@@ -8,25 +8,30 @@ class Cloud:
         self.speed = speed
         self.depth = depth
 
+
     def update(self):
         self.pos[0] += self.speed
 
     def render(self, surf, offset=(0, 0)):
-        render_pos = (self.pos[0] - offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)
-        surf.blit(self.img, (render_pos[0] % (surf.get_width() + self.img.get_width() - self.img.get_width()), render_pos[1] % (surf.get_height() + self.img.get_height() - self.img.get_height())))
+        render_pos = (self.pos[0]- offset[0] * self.depth, self.pos[1] - offset[1] * self.depth)
+
+        adjusted_x = (render_pos[0] + self.img.get_width()) % (surf.get_width() + self.img.get_width()) - self.img.get_width()
+        adjusted_y = render_pos[1] % (surf.get_height() + self.img.get_height()) - self.img.get_height()
+
+        surf.blit(self.img, (adjusted_x, adjusted_y))
 
 
 class Clouds:
-    def __init__(self, cloud_images, count=16):
+    def __init__(self, cloud_images, count=16, surf=None):
         self.clouds = []
 
         for i in range(count):
             self.clouds.append(Cloud(
-                (random.random() * 99999,
-                 random.random() * 99999,),
+                (random.random() * 9999,
+                 random.random() * 9999),
                  random.choice(cloud_images),
-                 random.random() * 0.01 + 0.02,
-                 random.random() * 0.1 + 0.075,
+                 random.random() * 0.1 + 0.02,
+                 random.random() * 0.3 + 0.7,
             ))
 
         self.clouds.sort(key=lambda cloud_object: cloud_object.depth)
